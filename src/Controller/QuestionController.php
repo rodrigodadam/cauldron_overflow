c82f9915ce0ce3d1c3871afb48e53ed950d73f6b
Rodrigo <rodrigodadam@gmail.com>
2021-11-01 17:16:51 +0000

    Add Annotations

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('What is this?');
+    }
+}
