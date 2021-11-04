76d54219ed77006b724d0e35c6016532cde75cdd
rodrigodadam <rodrigodadam@gmail.com>
2021-11-04 16:52:18 +0000

    Add mock answers

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,9 +22,15 @@ class QuestionController
      */
     public function show($question)
     {
-        return new Response(sprintf(
-            'Future page to show a question "%s"',
-            ucwords(str_replace('-', ' ', $question))
-        ));
+        $answers = [
+            'Make me happy with a drink',
+            'I think if you pay a trip will be better',
+            'Maybe.. just stay by my side'
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $question)),
+            'answers' => $answers,
+        ]);
     }
 }
